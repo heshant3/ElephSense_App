725bb3fc0d33116f07f1240ce16fd23cfdba58a6
Heshan Tharindu Kalubowila <55088720+heshant3@users.noreply.github.com>
2024-03-27 00:03:27 +0530

    firstday

---
+++ Screen/Analyses.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Analyses = () => {
+  return (
+    <View>
+      <Text>Analyses</Text>
+    </View>
+  );
+};
+
+export default Analyses;
+
+const styles = StyleSheet.create({});
